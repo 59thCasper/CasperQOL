440f5f966fc168dfd410597f6ded7db7e44a0392
Richard Green <death2003@blueyonder.co.uk>
2024-04-15 00:49:23 +0100

    test 2

--- CasperQOL/ShowGUI.cs
+++ CasperQOL/ShowGUI.cs
@@ -1,17 +1,15 @@
-﻿using UnityEngine;
+﻿using CasperQOL;
+using UnityEngine;
 
 public static class ShowGUI
 {
     public static bool shouldShow = false; // Control visibility of the GUI
 
-    private static bool toggleSpeed = false;
-    private static bool toggleHeadlight = false;
-    private static bool toggleProtection = false;
-
     private static GUIStyle guiStyleBox;
     private static GUIStyle guiStyleButton;
-    private static Texture2D buttonNormal;
-    private static Texture2D buttonHover;
+    private static Texture2D darkBackground;
+    private static Texture2D lightBackground;
+    private static Texture2D lightestBackground;
 
     static ShowGUI()
     {
@@ -21,27 +19,23 @@ public static class ShowGUI
     private static void InitializeStyles()
     {
         // Create textures
-        buttonNormal = new Texture2D(1, 1);
-        buttonNormal.SetPixel(0, 0, HexToColor("#5B4136"));
-        buttonNormal.Apply();
-
-        buttonHover = new Texture2D(1, 1);
-        buttonHover.SetPixel(0, 0, HexToColor("#947022"));
-        buttonHover.Apply();
+        darkBackground = CreateColorTexture("#161626");
+        lightBackground = CreateColorTexture("#303040");
+        lightestBackground = CreateColorTexture("#3A3A58");
 
         // Box style
         guiStyleBox = new GUIStyle(GUI.skin.box);
-        guiStyleBox.normal.background = buttonNormal;
-        guiStyleBox.normal.textColor = HexToColor("#F4B819");
+        guiStyleBox.normal.background = darkBackground;
+        guiStyleBox.normal.textColor = HexToColor("#5B4136");  // OrangeMid
         guiStyleBox.fontSize = 16;
         guiStyleBox.alignment = TextAnchor.UpperCenter;
 
         // Button style
         guiStyleButton = new GUIStyle(GUI.skin.button);
-        guiStyleButton.normal.textColor = HexToColor("#F4B819");
-        guiStyleButton.normal.background = buttonNormal;
-        guiStyleButton.hover.textColor = HexToColor("#FFFF16");
-        guiStyleButton.hover.background = buttonHover;
+        guiStyleButton.normal.textColor = HexToColor("#5B4136");  // OrangeMid
+        guiStyleButton.normal.background = lightBackground;
+        guiStyleButton.hover.textColor = HexToColor("#795933");  // OrangeBright
+        guiStyleButton.hover.background = lightBackground;
         guiStyleButton.fontSize = 14;
         guiStyleButton.alignment = TextAnchor.MiddleCenter;
     }
@@ -55,9 +49,10 @@ public static class ShowGUI
 
         GUI.Box(new Rect(xPos, yPos, 200, 150), "Casper's QOL Menu", guiStyleBox);
 
-        toggleSpeed = GUI.Toggle(new Rect(xPos + 10, yPos + 40, 180, 30), toggleSpeed, "Speed", guiStyleButton);
-        toggleHeadlight = GUI.Toggle(new Rect(xPos + 10, yPos + 75, 180, 30), toggleHeadlight, "Headlight", guiStyleButton);
-        toggleProtection = GUI.Toggle(new Rect(xPos + 10, yPos + 110, 180, 30), toggleProtection, "Protection", guiStyleButton);
+        // Update the toggles using SharedState
+        SharedState.speedToggle = GUI.Toggle(new Rect(xPos + 10, yPos + 40, 180, 30), SharedState.speedToggle, "Speed", guiStyleButton);
+        SharedState.lightToggle = GUI.Toggle(new Rect(xPos + 10, yPos + 75, 180, 30), SharedState.lightToggle, "Headlight", guiStyleButton);
+        SharedState.protectToggle = GUI.Toggle(new Rect(xPos + 10, yPos + 110, 180, 30), SharedState.protectToggle, "Protection", guiStyleButton);
     }
 
     private static Color HexToColor(string hex)
@@ -67,5 +62,14 @@ public static class ShowGUI
         byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
         byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
         return new Color32(r, g, b, 255);
+    }
+
+    private static Texture2D CreateColorTexture(string hex)
+    {
+        Color color = HexToColor(hex);
+        Texture2D texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, color);
+        texture.Apply();
+        return texture;
     }
 }
